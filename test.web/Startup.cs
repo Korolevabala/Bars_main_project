4dd75d451dd5f6112105300cbf5cfe7c6e2399ef
Korolevabala <pamitdura228@gmail.com>
2021-05-05 13:08:15 +0300

    пару добавлений

--- test.web/Startup.cs
+++ test.web/Startup.cs
@@ -9,7 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-
+using RabbitMQ;
+using RabbitMQ.Client;
 namespace test.web
 {
    public class Startup
---
+++ test.web/bb.session.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    'user'
+    'course',
+    'rezult',
+    
+)
