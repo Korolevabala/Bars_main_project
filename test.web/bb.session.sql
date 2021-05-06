19d9b99d1afca78fda4e868d7d212fa67009891e
Fazulov Rushan <68554563+Korolevabala@users.noreply.github.com>
2021-05-06 15:41:29 +0300

    добавил таблицу тестируемых
    1

--- test.web/bb.session.sql
+++ test.web/bb.session.sql
@@ -1,6 +1,5 @@
-CREATE TABLE users (
-    'user'
-    'course',
-    'rezult',
-    
+create table Users(
+	personal_account SERIAL PRIMARY KEY,
+	first_name varchar(30) not null,
+	code varchar(30) not null
 )
